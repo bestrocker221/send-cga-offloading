3cb236d7669eee9d61c8484f407672eea2e8aa5e
bestrocker221 <albertoscola221@gmail.com>
2018-09-23 17:00:44 +0200

    cga creation finished, now DAD

--- genCGA.py
+++ genCGA.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
-
 import hashlib, random, binascii
-#from struct import pack
 from ipaddress import IPv6Address
-# from cert import *
-# from scapy import *
-# from scapy.all import *
+
+'''
+Following CGA creation guidelines and pseudocode by
+
+https://en.wikipedia.org/wiki/Cryptographically_Generated_Address#CGA_generation_method
+
+'''
 
 def genCGA(sec, subnetPrefix, pubKey, extFields = b''):
 
@@ -16,7 +18,7 @@ def genCGA(sec, subnetPrefix, pubKey, extFields = b''):
 	pubk = binascii.hexlify(public_key)
 	#print("Public Key length: %s bits" % len(bin(int(pubk,16))[2:]))
 	
-	#label 1 start
+	#Label1 1 start
 	print("\n Starting phase 1 -> modifier creation..\n")
 	hash2, concatenate = "", b''
 	n = 0
@@ -35,30 +37,48 @@ def genCGA(sec, subnetPrefix, pubKey, extFields = b''):
 			digest = hashlib.sha1(concatenate).hexdigest()
 			hash2 = digest[0:14]
 			
-			#print("hash2 %s" % hash2)
-			#print("Digest: \n %s" %  digest)	#HEX   .digest() -> BYTES
-			#print("Hash2: (length: %s bits)\n %s" % (len(bin(int(hash2,16))[2:])+1, hash2) )
 	print("Modifier found: %s" % modifier)
 	print("Hash2 value:      %s" % hash2)
 	print("Rounds made:      %s" % n)
 
 	colCount = b'0' 				#collision count
 
-	#label2 start
-	print("\n Starting phase 2.. \n")
-	#how do i format subnet prefix and extfields?
+	#Label2 start
+	print("\n Starting phase 2 -> Interface Identifier creation.. \n")
+
 	#formatting ipv6 prefix
 	ipv6Prefix = IPv6Address(subnetPrefix + ":0000:0000:0000:0000") 	#add the 64 least significant bits
 	#just take the most 8*8=64 most significant bits of the whole 128bits ipv6 addr
 	ipv6Prefix = binascii.hexlify(ipv6Prefix.packed[0:8])
 	
 	concatenate = modifier + ipv6Prefix + colCount + pubk + extFields
-	print(concatenate)
+	#print(concatenate)
 	digest = hashlib.sha1(concatenate).hexdigest()
 	hash1 = digest[0:8] 		#8*8=64 most significant bits
 	intID = hash1 				#hash1 becomes Interface Identifier
+	
+	print("Hash1 value:     %s " % hash1)
+	#now intID[0] := intID[0] binary and 0x1c binary or (Sec << 5)
+	#print("intid pre:  %s" % intID)
 
-	return ("ciao", " stronzo")
+	intID_0 = (ord(intID[0]) & int("0x1c",16) | (sec << 5))
+	#print("%s -> %s " % (intID[0] ,intID_0))
+	#print("%s -> %s " % (intID_0, hex(intID_0)))
+	
+	intID = "".join(hex(ord(i))[2:] for i in intID[1:])
+	intID = hex(intID_0)[2:] + intID
+	print("Hash1 hex value: %s " % intID)
+
+	id1 = "{}:{}:{}:{}".format(intID[0:4],intID[4:8],intID[8:12],intID[12:16])
+
+	CGA = subnetPrefix + ":" + id1
+	print("\nCGA -> %s" % CGA)
+	#concatenate subnet prefix and ID
+
+	#DAD detection (hard part)
+	#TODO
+	
+	return ("", "")
 
 #just create a public-private rsa key pair before
 public_key = open("/home/bsodps/Dropbox/KTH/Research Methotolody/ResearchProject/test/devicea.test.pub.der","rb").read()
